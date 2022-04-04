114cb8c005b1005a2906e651b618958500f978f7
Ing. Jaime Agudelo <ingenieroagudelo@gmail.com>
2022-04-04 18:30:57 -0500

    Se crea el componente de LoginPage

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './general_styles.css';
-import App from './App';
+import { createRoot } from "react-dom/client";
+import React from "react";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import LoginPage from "./pages/login/app/LoginPage";
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <LoginPage />
+  </React.StrictMode>
 );
-
+reportWebVitals();
