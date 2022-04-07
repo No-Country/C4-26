40dfd9ca24d585415bdccfab48058488aa0a88b3
K-Marth <cbolanosrobles@gmail.com>
2022-04-06 20:55:36 -0500

    Agg Database

--- server/src/database/dbJobsMarket.sql
+++ server/src/database/dbJobsMarket.sql
@@ -14,13 +14,6 @@ CREATE TABLE "Users" (
   "country_name" varchar(255)
 );
 
-CREATE TABLE "Services" (
-  "id" int PRIMARY KEY,
-  "name_service" varchar,
-  "price" int,
-  "status" boolean
-);
-
 CREATE TABLE "Workers" (
   "worker_id" SERIAL PRIMARY KEY,
   "email" varchar(255) UNIQUE NOT NULL,
@@ -37,7 +30,7 @@ CREATE TABLE "Workers" (
 );
 
 CREATE TABLE "Seekers" (
-  "id" SERIAL PRIMARY KEY,
+  "seeker_id" SERIAL PRIMARY KEY,
   "email" varchar(255) UNIQUE NOT NULL,
   "full_name" varchar,
   "title_profile" varchar,
@@ -50,7 +43,7 @@ CREATE TABLE "Seekers" (
 );
 
 CREATE TABLE "Teamers" (
-  "id" SERIAL PRIMARY KEY,
+  "teamer_id" SERIAL PRIMARY KEY,
   "email" varchar(255) UNIQUE NOT NULL,
   "full_name" varchar,
   "title_profile" varchar,
@@ -62,30 +55,8 @@ CREATE TABLE "Teamers" (
   "country_name" varchar(255)
 );
 
-ALTER TABLE "Users" ADD FOREIGN KEY ("user_id") REFERENCES "Workers" ("worker_id");
+ALTER TABLE "Users" ADD FOREIGN KEY ("category") REFERENCES "Workers" ("worker_id");
 
-ALTER TABLE "Services" ADD FOREIGN KEY ("id") REFERENCES "Users" ("user_id");
+ALTER TABLE "Users" ADD FOREIGN KEY ("category") REFERENCES "Seekers" ("seeker_id");
 
-ALTER TABLE "Services" ADD FOREIGN KEY ("id") REFERENCES "Workers" ("worker_id");
-
-ALTER TABLE "Workers" ADD FOREIGN KEY ("worker_id") REFERENCES "Services" ("name_service");
-
-ALTER TABLE "Workers" ADD FOREIGN KEY ("worker_id") REFERENCES "Services" ("price");
-
-ALTER TABLE "Workers" ADD FOREIGN KEY ("worker_id") REFERENCES "Services" ("status");
-
-ALTER TABLE "Seekers" ADD FOREIGN KEY ("id") REFERENCES "Users" ("user_id");
-
-ALTER TABLE "Seekers" ADD FOREIGN KEY ("id") REFERENCES "Users" ("email");
-
-ALTER TABLE "Seekers" ADD FOREIGN KEY ("id") REFERENCES "Users" ("title_profile");
-
-ALTER TABLE "Seekers" ADD FOREIGN KEY ("id") REFERENCES "Users" ("profesional_profile_description");
-
-ALTER TABLE "Teamers" ADD FOREIGN KEY ("id") REFERENCES "Users" ("user_id");
-
-ALTER TABLE "Teamers" ADD FOREIGN KEY ("id") REFERENCES "Users" ("email");
-
-ALTER TABLE "Teamers" ADD FOREIGN KEY ("id") REFERENCES "Users" ("title_profile");
-
-ALTER TABLE "Teamers" ADD FOREIGN KEY ("id") REFERENCES "Users" ("profesional_profile_description");
+ALTER TABLE "Users" ADD FOREIGN KEY ("category") REFERENCES "Teamers" ("teamer_id");
