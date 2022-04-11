53f349e1e940dbd331199ee29d0c62ce7642ff55
Ing. Jaime Agudelo <ingenieroagudelo@gmail.com>
2022-04-11 09:36:20 -0500

    Actualizacion input en Comp. Register

--- client/src/pages/Auth/register/domain/RegisterPage.jsx
+++ client/src/pages/Auth/register/domain/RegisterPage.jsx
@@ -1,21 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import "../../login/sass/style.scss";
 
 import imgPerfilM from "../../../../assets/img/naranja1.png";
-import imgPerfilF from "../../../../assets/img/avatar2.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserPlus } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
+import { countrys } from "../infrastructure/_countries";
 
 const RegisterPage = () => {
   // ===================== Block 0 - Variables de estado =============================
 
-  const [dialogVisible, setDialogVisible] = useState(true);
   const [userDataregister, setUserDataregister] = useState([]);
+  const [dataCountry, setDataCountry] = useState([]);
+
+  // ===================== Block 1 - Variables y constantes ==========================
+
+  const UrlCountry = "https://restcountries.com/v2/";
 
   // =================================================================================
 
-  // ===================== Block 1 - Metodos y Funciones =============================
+  // ===================== Block 2 - Metodos y Funciones =============================
 
   const handleChange = (e) => {
     setUserDataregister({
@@ -24,9 +29,21 @@ const RegisterPage = () => {
     });
   };
 
-  const handleCickOpen = () => {
-    setDialogVisible(!dialogVisible);
+  console.log(userDataregister);
+
+  const getAllCountry = async () => {
+    try {
+      setDataCountry(
+        await axios.get(UrlCountry + "all").then((country) => country.data)
+      );
+    } catch (error) {
+      console.log(error);
+    }
   };
+
+  useEffect(() => {
+    getAllCountry();
+  }, []);
 
   // =================================================================================
 
@@ -59,6 +76,7 @@ const RegisterPage = () => {
 
           <form className="">
             <div className="content-hr center">
+              <label className="name-input">Nombre Completo</label>
               <input
                 type="text"
                 name="full-name"
@@ -68,15 +86,33 @@ const RegisterPage = () => {
                 onChange={handleChange}
               />
 
-              <input
-                type="number"
-                name="phone-num"
-                placeholder="Teléfono"
-                className="form-control-reg reg-input-field"
-                autoComplete="off"
-                onChange={handleChange}
-              />
+              <label className="name-input">Número de contacto</label>
+              <div className="numberPhone">
+                <select
+                  className="form-control-reg cod-input-field"
+                  name="code-country"
+                  placeholder="Código"
+                  onChange={handleChange}
+                >
+                  <option>Seleccionar país</option>
+                  {countrys.map((country) => (
+                    <option key={country.code} value={country.label}>
+                      {`${country.code}(+${country.phone})`}
+                    </option>
+                  ))}
+                </select>
 
+                <input
+                  type="number"
+                  name="phone-num"
+                  placeholder="Teléfono"
+                  className="form-control-reg num-input-field"
+                  autoComplete="off"
+                  onChange={handleChange}
+                />
+              </div>
+
+              <label className="name-input">Perfil profesional</label>
               <input
                 type="text"
                 name="title-perfil"
@@ -86,6 +122,7 @@ const RegisterPage = () => {
                 onChange={handleChange}
               />
 
+              <label className="name-input">Correo electrónico</label>
               <input
                 type="email"
                 name="email"
@@ -95,6 +132,7 @@ const RegisterPage = () => {
                 onChange={handleChange}
               />
 
+              <label className="name-input">Contraseña</label>
               <input
                 type="password"
                 name="password"
@@ -104,41 +142,32 @@ const RegisterPage = () => {
                 onChange={handleChange}
               />
 
-              <input
-                type="text"
-                name="category"
-                placeholder="Categoria"
+              <label className="name-input">Categoría</label>
+              <select
                 className="form-control-reg reg-input-field"
-                autoComplete="off"
-                onChange={handleChange}
-              />
-
-              <input
-                type="text"
                 name="code-country"
-                placeholder="Codigo del País"
-                className="form-control-reg reg-input-field"
-                autoComplete="off"
-                onChange={handleChange}
-              />
+                placeholder="Codigo y Pais"
+              >
+                <option>Seleccionar una categoria</option>
+                <option>Seekers</option>
+                <option>Teamers</option>
+                ))
+              </select>
 
-              <input
-                type="text"
-                name="country-name"
-                placeholder="País de Residencia"
+              <label className="name-input">Pais de Residencia</label>
+              <select
                 className="form-control-reg reg-input-field"
-                autoComplete="off"
+                name="code-country"
+                placeholder="Codigo y Pais"
                 onChange={handleChange}
-              />
-
-              <input
-                type="file"
-                name="photo-perfil"
-                placeholder="Agregha una foto del perfil"
-                className="form-control-reg reg-input-field"
-                autoComplete="off"
-                onChange={handleChange}
-              />
+              >
+                <option>Seleccionar país</option>
+                {countrys.map((country) => (
+                  <option key={country.code} value={country.label}>
+                    {`${country.label} (+${country.phone})`}
+                  </option>
+                ))}
+              </select>
             </div>
           </form>
 
