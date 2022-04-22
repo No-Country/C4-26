818a1083361b9b78dadbb181deb41e1f3d695757
Ing. Jaime Agudelo <ingenieroagudelo@gmail.com>
2022-04-21 19:04:20 -0500

    About Me - Cambio de descripcion

--- client/src/pages/about_me/domain/AboutMe.jsx
+++ client/src/pages/about_me/domain/AboutMe.jsx
@@ -3,12 +3,7 @@ import "./about.scss";
 
 import imgAboutMe from "../../../assets/img/about/aboutoffice.jpg";
 import aboutPres from "../../../assets/img/help/womanProfileWeb.png";
-import orange from "../../../assets/img/naranja1.png";
 
-import { Card } from "@mui/material";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
-import { CardActionArea } from "@mui/material";
 import CardComponent from "../../../components/card/CardComponent";
 
 const AboutMe = () => {
@@ -30,9 +25,9 @@ const AboutMe = () => {
                 <h1>We Provide Quality Services & Quality People</h1>
               </div>
               <p className="about-text">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Delectus eius distinctio odit, magni magnam qui ex perferendis
-                vitae!
+                JobsMarket is an ecommerce that focuses on providing our
+                customers with services from the best professionals at their
+                disposal.
               </p>
             </div>
 
@@ -51,30 +46,7 @@ const AboutMe = () => {
         <hr className="line-divisor" />
       </div>
       <div className="container-core-values">
-        <Card className="card" sx={{ maxWidth: 350 }}>
-          <CardActionArea>
-            <img className="img-card" src={orange} alt="img-orang" />
-            <CardContent>
-              <Typography
-                className="title-card"
-                gutterBottom
-                variant="h5"
-                component="div"
-              >
-                Responsability
-              </Typography>
-              <Typography
-                className="paragraph"
-                variant="body2"
-                color="text.secondary"
-              >
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel
-                nesciunt aliquam amet possimus, voluptas quasi debitis quod
-                vitae natus? Voluptates, nihil!
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
+        <CardComponent subTitle="Responsability" />
         <CardComponent subTitle="Confidence" />
         <CardComponent subTitle="Security" />
       </div>
